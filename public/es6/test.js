066a5513e016fb6ebd93a50961abd40d01ce907d
Marx <domah88@gmail.com>
2017-02-10 13:13:13 -0600

    Added updated files in root folder.

---
+++ public/es6/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// es6 feature: block-scoped "let" declaration
+const sentences = [
+  { subject: 'JavaScript', verb: 'is', object: 'tough' },
+  { subject: 'Elephants', verb: 'was', object: 'here' },
+];
+
+// es6 feature: object destructuring
+function say({ subject, verb, object }) {
+  //es6 feature: template strings
+  console.log(`${subject} ${verb} ${object}`);
+}
+
+// es6 feature: for..of
+for (let s of sentences) {
+  say(s);
+}
